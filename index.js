9ab8d0b11788ad92ce3e59129663b45002b0e512
Jennifer Haddock <jentx123@hotmail.com>
2017-11-27 00:53:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function showRepositories(event, data) {
+function displayRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
   console.log(repos)
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
@@ -8,7 +8,7 @@ function showRepositories(event, data) {
 function getRepositories() {
   var username = document.getElementById("username").value;
   const req = new XMLHttpRequest()
-  req.addEventListener("load", showRepositories);
+  req.addEventListener("load", displayRepositories);
   req.open("GET", 'https://api.github.com/users/' + username + '/repos')
   req.send()
 }
