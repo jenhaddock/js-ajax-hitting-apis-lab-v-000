e0bb986e722ce2a636c650814090d55a83741e57
Jennifer Haddock <jentx123@hotmail.com>
2017-11-27 01:33:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function getRepositories() {
   const username = document.getElementById("username").value;
   const req = new XMLHttpRequest()
   req.addEventListener("load", displayRepositories);
-  req.open("GET", 'https://api.github.com/users/${username}/repos')
+  req.open("GET", 'https://api.github.com/users/' + username + '/repos')
   req.send()
 }
 
